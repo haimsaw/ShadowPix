6187d3c9ce57eb5cd5117945e10ed8c6875242fa
hasawday <haimsaw@gmail.com>
2020-01-08 21:45:02 +0200

    add concurrency

--- GenareteGlobal.py
+++ GenareteGlobal.py
@@ -29,10 +29,10 @@ class Telemetry:
         self.get_lit_map = 0
         self.num_of_mesurments = 0
 
-    def update_messurment(self, start_iteration_time, take_step_time, evel_f_time, end_iteration_time ):
-        self.iteration_duration += end_iteration_time-start_iteration_time
-        self.take_step_duration += take_step_time-start_iteration_time
-        self.eval_f_duration += evel_f_time-take_step_time
+    def update_messurment(self, start_iteration_time, take_step_time, evel_f_time, end_iteration_time):
+        self.iteration_duration += end_iteration_time - start_iteration_time
+        self.take_step_duration += take_step_time - start_iteration_time
+        self.eval_f_duration += evel_f_time - take_step_time
         self.num_of_mesurments += 1
 
     def update_get_lit_map(self, duration):
@@ -40,8 +40,9 @@ class Telemetry:
 
     def __str__(self):
         return "telemetry iter:{0:03f} step:{1:03f} eval={2:03f} lit_map={3:03f} niter={4}".format(
-            self.iteration_duration/self.num_of_mesurments, self.take_step_duration/self.num_of_mesurments,
-            self.eval_f_duration/self.num_of_mesurments, self.get_lit_map/self.num_of_mesurments, self.num_of_mesurments)
+            self.iteration_duration / self.num_of_mesurments, self.take_step_duration / self.num_of_mesurments,
+            self.eval_f_duration / self.num_of_mesurments, self.get_lit_map / self.num_of_mesurments,
+            self.num_of_mesurments)
 
 
 class State:
@@ -90,8 +91,8 @@ class State:
 
     def take_step_random(self, telemetry):
         while True:
-            i = random.randint(0, image_size-1)
-            j = random.randint(0, image_size-1)
+            i = random.randint(0, image_size - 1)
+            j = random.randint(0, image_size - 1)
             is_add = random.randint(0, 1) == 0
             new_val = self.heightfield[i][j] + 5 if is_add else self.heightfield[i][j] - 5
             if 0 <= new_val <= 10:
@@ -132,13 +133,45 @@ class State:
         self.take_step_random(telemetry)
         return self
 
-#class
+
+class StateEvaluator:
+    def __init__(self, images):
+        self.gradient_kernel = numpy.asarray([[0, 1, 0],
+                                         [1, -4, 1],
+                                         [0, 1, 0]])
+
+        self.gaussian_kernel = numpy.asarray([[1, 4, 7, 4, 1],
+                                         [4, 16, 26, 16, 4],
+                                         [7, 26, 41, 26, 7],
+                                         [4, 16, 26, 16, 4],
+                                         [1, 4, 7, 4, 1]]) / 273
+
+        self.gaus_grad_kernel = signal.convolve2d(self.gaussian_kernel, self.gradient_kernel, mode="same")
+
+        self.images = images
+        self.edges = {direction: signal.convolve2d(images[direction], self.gaus_grad_kernel, mode="same")
+                      for direction in directions}
+
+    def val_for_direction(self, lit_map, direction):
+        res = 0
+        res += StateEvaluator.norma(signal.convolve2d(lit_map, self.gaussian_kernel, mode="same"), self.images[direction])
+        res += StateEvaluator.norma(signal.convolve2d(lit_map, self.gaus_grad_kernel, mode="same"), self.edges[direction]) * 1.5
+        return res
+
+    def val_for_heightfield(self, heightfield):
+        return StateEvaluator.norma(signal.convolve2d(heightfield, self.gradient_kernel, mode="same"),
+                     numpy.zeros((image_size, image_size))) * 0.001
+
+    @staticmethod
+    def norma(im1, im2):
+        return numpy.sum((im1 - im2) ** 2)
+
 
 def main():
     images = get_images()
-    func = get_f_to_minimize(images)
+    state_evaluator = StateEvaluator(images)
     fig = plt.figure(figsize=(10, 10))
-    heightfield = my_optimize(func, images, fig)
+    heightfield = my_optimize(state_evaluator, images, fig)
 
     save_res(heightfield, images, fig)
     plt.show()
@@ -158,56 +191,33 @@ def save_res(stet, images, fig):
 
 
 def get_images():
-        #images.append(numpy.asarray([[1 if i % 5 == 0 else 0 for i in range(image_size)] for _ in range(image_size)]))
-        #  images.append(numpy.random.rand(image_size, image_size))
+    # images.append(numpy.asarray([[1 if i % 5 == 0 else 0 for i in range(image_size)] for _ in range(image_size)]))
+    #  images.append(numpy.random.rand(image_size, image_size))
 
-    get_im = lambda direction: numpy.array(Image.open("img{0}.jpg" .format(direction)).
-                                  convert("L").crop((0, 0, image_size, image_size)))/255
+    get_im = lambda direction: numpy.array(Image.open("img{0}.jpg".format(direction)).
+                                           convert("L").crop((0, 0, image_size, image_size))) / 255
     images = {direction: get_im(direction) for direction in directions}
     return images
 
 
-def get_f_to_minimize(images):
-    gradient_kernel = numpy.asarray([[0, 1, 0],
-                                    [1, -4, 1],
-                                    [0, 1, 0]])
+def f_to_minimize(pool, state_evaluator, state):
 
-    gaussian_kernel = numpy.asarray([[1, 4, 7, 4, 1],
-                                    [4, 16, 26, 16, 4],
-                                    [7, 26, 41, 26, 7],
-                                    [4, 16, 26, 16, 4],
-                                    [1, 4, 7, 4, 1]]) / 273
+    args = [(state.get_lit_map(direction), direction) for direction in directions]
 
-    gaus_grad_kernel = signal.convolve2d(gaussian_kernel, gradient_kernel, mode="same")
-    norma = lambda im1, im2: numpy.sum((im1-im2)**2)
-    edges = {direction: signal.convolve2d(images[direction], gaus_grad_kernel, mode="same") for direction in directions}
-    # pool = multiprocessing.Pool(processes=num_of_images)
+    async_res = pool.starmap_async(state_evaluator.val_for_direction, args)
+    res = state_evaluator.val_for_heightfield(state.heightfield)
+    res += sum(async_res.get())
 
-    def res_for_direction(lit_map, direction):
-        res = 0
-        res += norma(signal.convolve2d(lit_map, gaussian_kernel, mode="same"), images[direction])
-        res += norma(signal.convolve2d(lit_map, gaus_grad_kernel, mode="same"), edges[direction]) * 1.5
-        return res
-
-    def f(state):
-
-        args = [(state.get_lit_map(direction), direction) for direction in directions]
-        res = sum(itertools.starmap(res_for_direction, args))
-
-        res += norma(signal.convolve2d(state.heightfield, gradient_kernel, mode="same"),
-                     numpy.zeros((image_size, image_size)))*0.001
-        return res
-
-    return f
+    return res
 
 
-def my_optimize(f, images, fig):
-    inital_guss = State()
-
+def my_optimize(state_evaluator, images, fig):
     def callback(iteration_num, state, telemetry):
-        if iteration_num % 1000 == 0:  # todo this
+        if iteration_num % 50 == 0:  # todo this
+
             save_res(state, images, fig)
             print(telemetry)
+
     '''
     def no_minimizer(fun, x0, args, jac, hess, hessp,
                       bounds, constraints,
@@ -220,17 +230,19 @@ def my_optimize(f, images, fig):
     #res = optimize.basinhopping(f, x0=inital_guss, take_step=mytakestep, disp=True, minimizer_kwargs=minimizer_kwargs, niter=10**7, niter_success=1000).x
     '''
 
-    res = simulated_annealing(f, 10**7, 100000, inital_guss, callback)
+    res = simulated_annealing(state_evaluator, 10 ** 7, 100000, State(), callback)
     return res
 
 
-def simulated_annealing(f, niter, niter_success, x0, callback):
-    state = x0
+def simulated_annealing(state_evaluator, niter, niter_success, state, callback):
     telemetry = Telemetry()
-    val = f(state)
+    pool = multiprocessing.Pool(processes=num_of_images)
+
+    val = f_to_minimize(pool, state_evaluator, state)
     global_min_val = val
     global_min_state = state
     success_iter = 0
+
     for iteration_num in range(niter):
         if niter_success < success_iter:
             break
@@ -238,10 +250,10 @@ def simulated_annealing(f, niter, niter_success, x0, callback):
         temp = niter - iteration_num - 1  # todo, check this
         candidate_state = state.copy().take_step(telemetry)
         take_step_time = time.time()
-        candidate_val = f(candidate_state)
+        candidate_val = f_to_minimize(pool, state_evaluator, candidate_state)
         evel_f_time = time.time()
 
-        if candidate_val < val or random.random() <= math.exp((val-candidate_val)/temp):
+        if candidate_val < val or random.random() <= math.exp((val - candidate_val) / temp):
             state = candidate_state
             val = candidate_val
         print("iteration={0} val={1} global val={2} success_iter={3}".
