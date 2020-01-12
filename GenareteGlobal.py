c59f34dced0642ddba7727c923be40b514fb660a
hasawday <haimsaw@gmail.com>
2020-01-12 19:03:41 +0200

    change cuncurrency to be for multiple steps

--- GenareteGlobal.py
+++ GenareteGlobal.py
@@ -6,17 +6,18 @@ from scipy.signal import convolve2d
 from random import randint, random
 from PIL import Image
 import matplotlib.pyplot as plt
-from time import time
+from time import time, sleep
 from copy import deepcopy
-import multiprocessing
+from multiprocessing import Pool, cpu_count
 import numpy
-
+import os
 
 # todo any number of images
 
-image_size = 300  # max in article 200/0.5=400
+image_size = 400  # max in article 200/0.5=400
 
 num_of_images = 4
+num_of_cores = cpu_count()
 directions = ["+x", "+y", "-x", "-y"][:num_of_images]
 light_angle = radians(30)
 
@@ -25,35 +26,29 @@ class Telemetry:
     def __init__(self):
         self.iteration_duration = 0
         self.take_step_duration = 0
-        self.eval_f_duration = 0
-        self.get_lit_map = 0
+        self.get_accepted_steps_time = 0
         self.num_of_mesurments = 0
-        self.total_eval_f_duration = 0
 
-    def update_messurment(self, start_iteration_time, take_step_time, evel_f_time, end_iteration_time):
+    def update_messurment(self, start_iteration_time, take_step_time, get_accepted_steps_time, end_iteration_time):
         self.iteration_duration += end_iteration_time - start_iteration_time
         self.take_step_duration += take_step_time - start_iteration_time
-        self.eval_f_duration += evel_f_time - take_step_time
+        self.get_accepted_steps_time += get_accepted_steps_time - take_step_time
         self.num_of_mesurments += 1
 
-    def update_get_lit_map(self, duration):
-        self.get_lit_map += duration
 
-    def update_total_eval_f_duration(self, duration):
-        self.total_eval_f_duration += duration
 
     def __str__(self):
 
-        return "\ttelemetry iter:{:03f} step:{:03f} eval={:03f} total_eval={:03f} lit_map={:03f} niter={}".format(
+        return "\ttelemetry iter:{:03f} step:{:03f} get_accepted_steps_time={:03f} niter={}".format(
             self.iteration_duration / self.num_of_mesurments, self.take_step_duration / self.num_of_mesurments,
-            self.eval_f_duration / self.num_of_mesurments, self.total_eval_f_duration / self.num_of_mesurments,
-            self.get_lit_map / self.num_of_mesurments, self.num_of_mesurments)
+            self.get_accepted_steps_time / self.num_of_mesurments, self.num_of_mesurments)
 
 
 class State:
     def __init__(self):  # todo pass step taker
         self.heightfield = numpy.zeros((image_size, image_size), dtype=int)
         self.lit_maps = {direction: numpy.ones((image_size, image_size), dtype=int) for direction in directions}
+        self.val = None
 
     def get_lit_map(self, direction):
         return self.lit_maps[direction]
@@ -96,49 +91,42 @@ class State:
                 for iter_j in range(image_size):
                     next_shadow_height = self.update_lit_pixel(i, iter_j, next_shadow_height, direction)
 
-    def take_step_random(self, telemetry):
+    def get_step_random(self):
         while True:
             i = randint(0, image_size - 1)
             j = randint(0, image_size - 1)
             change = randint(-5, 5)
             new_val = self.heightfield[i][j] + change
             if change != 0 and 0 <= new_val <= 10:
-                self.heightfield[i][j] = new_val
-                start = time()
-                self.update_lit_maps(i, j)
-                telemetry.update_get_lit_map(time() - start)
-                break
+                return {"i": i, "j": j, "height": new_val}
 
-    def take_step_all(self, telemetry):
-        pass  # todo
-        '''
-        class MyTakeStepAll:
-            def __init__(self):
-                self.y = self.x = image_size - 1
-                self.is_add = False
-        
-            def __call__(self, state):
-                while True:
-                    self.x = (self.x + 1) % image_size
-                    if self.x == 0:
-                        self.y = (self.y + 1) % image_size
-                    if self.x == self.y == 0:
-                        self.is_add = not self.is_add
-        
-                    new_val = state[self.x][self.y] + 5 if self.is_add else state[self.x][self.y] - 5
-                    if 0 <= new_val <= 10:
-                        new_state = numpy.copy(state)
-                        new_state[self.x][self.y] = new_val
-                        break
-                return new_state
-        
-            def get_niter_success(self):
-                return image_size*image_size*2
-        '''
+    def get_steps(self, num_of_steps):
+        steps = []
+        step_radios = 20
+        while len(steps) < num_of_steps:
+            step = self.get_step_random()
+            should_accept = True
+            for existine_step in steps:
+                if abs(existine_step["i"] - step["i"]) <= step_radios or abs(existine_step["j"] - step["j"]) <= step_radios:
+                    should_accept = False
+                    break
+            if should_accept:
+                steps.append(step)
+        return steps
 
-    def take_step(self, telemetry):
-        self.take_step_random(telemetry)
-        return self
+    def get_val(self, state_evaluator):
+        if self.val is not None:
+            return self.val
+
+        args = [(self.get_lit_map(direction), direction) for direction in directions]
+        self.val = sum(itertools.chain(itertools.starmap(state_evaluator.val_for_direction, args),
+                                      [state_evaluator.val_for_heightfield(self.heightfield)]))
+        return self.val
+
+    def apply_step(self, step):
+        self.heightfield[step["i"]][step["j"]] = step["height"]
+        self.update_lit_maps(step["i"], step["j"])
+        self.val = None
 
 
 class StateEvaluator:
@@ -160,16 +148,13 @@ class StateEvaluator:
                       for direction in directions}
 
     def val_for_direction(self, lit_map, direction):
-        start = time()
         res = 0
         res += StateEvaluator.norma(convolve2d(lit_map, self.gaussian_kernel, mode="same"), self.images[direction])
         res += StateEvaluator.norma(convolve2d(lit_map, self.gaus_grad_kernel, mode="same"), self.edges[direction]) * 1.5
-        return res, time() - start
+        return res
 
     def val_for_heightfield(self, heightfield):
-        start = time()
-        return StateEvaluator.norma(convolve2d(heightfield, self.gradient_kernel, mode="same")) * 0.001, \
-               time() - start
+        return StateEvaluator.norma(convolve2d(heightfield, self.gradient_kernel, mode="same")) * 0.001
 
     @staticmethod
     def norma(im1, im2=None):
@@ -213,6 +198,7 @@ def get_images():
     return images
 
 
+'''
 def f_to_minimize_concurrent(pool, state_evaluator, state, telemetry):
     res = calc_times = 0
     args = [(state.get_lit_map(direction), direction) for direction in directions]
@@ -228,36 +214,46 @@ def f_to_minimize_concurrent(pool, state_evaluator, state, telemetry):
         telemetry.update_total_eval_f_duration(calc_times)
     return res
 
-
-def f_to_minimize_non_concurrent(pool, state_evaluator, state, telemetry):
-    res = calc_times = 0
-    args = [(state.get_lit_map(direction), direction) for direction in directions]
-    evaluations = itertools.chain(itertools.starmap(state_evaluator.val_for_direction, args),
-                                  [state_evaluator.val_for_heightfield(state.heightfield)])
-
-    for evaluation in evaluations:
-        res += evaluation[0]
-        calc_times += evaluation[1]
-    if telemetry is not None:
-        telemetry.update_total_eval_f_duration(calc_times)
-    return res
+'''
 
 
 def my_optimize(state_evaluator, images):
     def callback(iteration_num, state, telemetry):
-        if iteration_num % 1000 == 0:  # todo this
+        if iteration_num % 50 == 0:  # todo this
             save_res(state, images)
             print(telemetry)
 
-    with multiprocessing.Pool(processes=num_of_images) as pool:
-        return simulated_annealing(state_evaluator, f_to_minimize_concurrent, 10 ** 7, 10 ** 5, State(),
-                                   callback, pool, 1)
+    with Pool(processes=num_of_cores) as pool:
+        return simulated_annealing(state_evaluator, 10 ** 7, 10 ** 5, State(), callback, pool, 1)
 
 
-def simulated_annealing(state_evaluator, f, niter, niter_success, state, callback, pool, initial_temperature):
+def is_step_accepted(state, step, state_evaluator, temp):
+    candidate = state.copy()
+    candidate.apply_step(step)
+    metopolis_test = exp((state.get_val(state_evaluator) - candidate.get_val(state_evaluator)) / temp)
+    return random() <= metopolis_test, step
 
-    val = f(pool, state_evaluator, state, None)
-    global_min_val = val
+
+def simulated_annealing(state_evaluator, niter, niter_success, state, callback, pool, initial_temperature):
+
+    def get_accepted_steps(candidate_steps, state):
+        args = [(state, step, state_evaluator, temp) for step in candidate_steps]
+        copy = state.copy()
+
+        for res, step in pool.starmap_async(is_step_accepted, args).get():
+            if res:
+                copy.apply_step(step)
+        return copy
+
+    def get_accepted_steps_non_concurrent(candidate_steps, state):
+        args = [(state, step, state_evaluator, temp) for step in candidate_steps]
+        copy = state.copy()
+
+        for res, step in itertools.starmap(is_step_accepted, args):
+            if res:
+                copy.apply_step(step)
+        return copy
+
     global_min_state = state
     min_time_at_top = 0
     telemetry = Telemetry()
@@ -267,28 +263,24 @@ def simulated_annealing(state_evaluator, f, niter, niter_success, state, callbac
             break
         start_iteration_time = time()
         temp = (niter - iteration_num)/niter*initial_temperature
-        candidate_state = state.copy().take_step(telemetry)
+        candidate_steps = state.get_steps(num_of_cores)
+
         take_step_time = time()
-        candidate_val = f(pool, state_evaluator, candidate_state, telemetry)
-        evel_f_time = time()
-        metopolis_test = exp((val - candidate_val) / temp)
+        state = get_accepted_steps_non_concurrent(candidate_steps, state)
 
-        print("iteration={} val={} candidate_val={} metopolis={:.2f} min={} time_at_top={} temp={:.2f}".
-              format(iteration_num, int(val), int(candidate_val), metopolis_test, int(global_min_val), min_time_at_top, temp))
+        get_accepted_steps_time = time()
 
-        if random() <= metopolis_test:
-            state = candidate_state
-            val = candidate_val
+        print("iteration={} val={} min={} time_at_top={} temp={:.2f}".
+              format(iteration_num, int(state.get_val(state_evaluator)), int(global_min_state.get_val(state_evaluator)), min_time_at_top, temp))
 
-        if val < global_min_val:
-            global_min_val = val
+        if state.get_val(state_evaluator) < global_min_state.get_val(state_evaluator):
             global_min_state = state
             min_time_at_top = 0
 
         else:
             min_time_at_top += 1
 
-        telemetry.update_messurment(start_iteration_time, take_step_time, evel_f_time, time())
+        telemetry.update_messurment(start_iteration_time, take_step_time, get_accepted_steps_time, time())
 
         callback(iteration_num, global_min_state, telemetry)
     return global_min_state
